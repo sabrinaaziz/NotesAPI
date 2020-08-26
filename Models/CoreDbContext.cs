bf895ea4c9ad73568ca403ae4f9340207bef7028
Sabrina Aziz <sabrinagotmail@gmail.com>
2020-08-26 22:14:50 +0100

    Notes API in .net core

---
+++ Models/CoreDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NotesApi.Models
+{
+    public partial class CoreDbContext : DbContext
+    {
+        public CoreDbContext()
+        {
+        }
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Notes> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           /* if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=NotesDB;Trusted_Connection=True;");
+            }*/
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
